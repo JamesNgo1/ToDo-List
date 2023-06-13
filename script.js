98212ee4e2b02aae8d61b5b2bfb1ded964c660c2
James Ngo <Jamestngo02@gmail.com>
2023-06-13 03:20:36 -0400

    Add responsive add ons to list and button

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function random(){
-    console.log("java script is working");
-}
+const input = document.getElementById("input-textBox");
+const ul = document.querySelector(".list-container");
 
-random();
+
+function addTask(){
+    //case input is empty and send alert message to user
+    if(input.value === "") alert("Please enter a task");
+
+    else{
+        //Create li varaible to create new element and append to ul list
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        ul.appendChild(li);
+        //append the x (span) on the li parent
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+
+    //after user click add we reset the input to clear
+    input.value = "";
+}//end of function
+
+
